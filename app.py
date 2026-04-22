049f4eeb2d48a3fc05509c06423b041421cfd745
AvlrXD <teracopy.contact@gmail.com>
2026-04-22 17:34:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -505,9 +505,10 @@ async def logo_post(request: web.Request) -> web.Response:
 
 
 def main():
-    """Ponto de entrada para executar a aplicação."""
     app = create_app()
-    web.run_app(app, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    web.run_app(app, host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
